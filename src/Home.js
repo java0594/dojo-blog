48421580f2bac1efa65af6c45e9521173e1fc6c9
java0594 <juanvasquez0594@gmail.com>
2022-08-30 16:07:29 -0500

    hook mario a luigi

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,19 @@
+import { useState } from "react";
 const Home  = () => {
+    //let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
     const handleClick = () => {
-        console.log('Hello, ninjas');
+        setName('luigi');
+        setAge(30);
     }
-    const handleClickAgain = (name) => {
-        alert('hello' + name);
-    }
+    
     
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} old </p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={() => handleClickAgain('mario')}>Click me again</button>
         </div>
      );
 }
